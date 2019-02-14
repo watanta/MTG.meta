69107c590ae25d4a6139e0c9ced27bed99020e40
watanabe <watanabe101watanabe@yahoo.co.jp>
2019-02-14 10:52:43 +0900

    pagenation実装した#4

--- MTG.meta.py
+++ MTG.meta.py
@@ -1,10 +1,15 @@
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
+from flask_bootstrap import Bootstrap
+from flask_paginate import Pagination, get_page_parameter, get_page_args
+
 
 app = Flask(__name__) #インスタンス生成
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/mtga'
 mongo = PyMongo(app)
+bootstrap = Bootstrap(app)
+
 
 
 
@@ -12,14 +17,29 @@ mongo = PyMongo(app)
 def hello():
   return "Hello World!"
 
+def get_decks(offset=0, per_page=10):
+    return mongo.db.decks.find().skip(offset).limit(per_page)
 
-@app.route("/decklist")
+@app.route("/decklist", methods=['GET'])
 def decklist():
-    decks = []
-    decks = mongo.db.decks.find()
 
-    return render_template('decklist.html', decks=decks)
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+
+    total = mongo.db.decks.count()
+    pagination_decks = get_decks(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template('decklist.html',
+                           decks=pagination_decks,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           )
+
+
+
 
 if __name__ == "__main__":
     # webサーバー立ち上げ
-    app.run()
+    app.run(debug=True)
